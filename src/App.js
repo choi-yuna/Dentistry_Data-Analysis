717f254cb7aecfd8a52cc3731a77ffcc34d3e2d2
DESKTOP-BSAE3OV\fasol <wlsdn4348@naver.com>
2024-08-26 10:36:33 +0900

    [FE-02]: 상단바 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import TopBar from './components/topbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <TopBar/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
