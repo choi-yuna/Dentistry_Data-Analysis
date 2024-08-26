b7c07c0fb5926a58a6f6b4461c79c395093b0ffb
choi-yuna <mjung0226@naver.com>
2024-08-26 17:17:33 +0900

    [FE-05]: 컴포넌트 컨테이너 조정

--- src/dataQualityView/dataQuality.jsx
+++ src/dataQualityView/dataQuality.jsx
@@ -1,44 +1,40 @@
-// src/dataQualityView/dataQuality.js
 import React from 'react';
 import styled from 'styled-components';
 import TopBar from '../components/topbar';
 import MenuBar from '../components/menubar';
 import FormComponent from '../components/FormComponent';
 
-// Styled Components 정의
 const AppContainer = styled.div`
   display: flex;
-  flex-direction: column; 
-  height: 100vh; 
+  flex-direction: column;
+  height: 100vh;
 `;
 
 const MainContent = styled.div`
   display: flex;
+  flex-direction: row;
   flex: 1;
 `;
 
-const Sidebar = styled.div`
-  width: 250px;
-  flex-shrink: 0; 
-`;
-
-const Content = styled.div`
-  flex: 1; 
-  padding: 20px;
+const ContentCtn = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex: 1;
+  padding: 20px; 
 `;
 
 // DataQualityView 컴포넌트 정의
 const DataQualityView = () => {
   return (
     <AppContainer>
-      <TopBar />
+     <TopBar />
       <MainContent>
-        <Sidebar>
-          <MenuBar />
-        </Sidebar>
-        <Content>
+        <MenuBar />
+        <ContentCtn>
           <FormComponent />
-        </Content>
+        </ContentCtn>
       </MainContent>
     </AppContainer>
   );
