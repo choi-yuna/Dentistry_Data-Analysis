f5b7f969810a726750cd54f0f27f1b8a7f6a94ac
choi-yuna <mjung0226@naver.com>
2024-11-14 11:16:12 +0900

    [FE-91]: TotalFileStatus 컴포넌트 구현

--- src/dataCompositionView/dataCompositionView.jsx
+++ src/dataCompositionView/dataCompositionView.jsx
@@ -1,7 +1,49 @@
-import React, { useState, useContext } from 'react';
+// DataCompositionView.jsx
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import TopBar from '../components/topbar';
 import MenuBar from '../components/menubar';
+import TotalFileStatus from '../components/TotalFileStatus';
+
+const DataCompositionView = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  // TotalFileStatus 전달 더미데이터
+  const background = '#F4F4FF'; 
+  const totalFiles = '총파일 수';
+  const uploadDate = '2023-11-14';
+  const fileCount = 3400;
+  const color = '#051C91';
+
+  return (
+    <AppContainer>
+      <TopBar />
+      <MainContent>
+        <MenuBar collapsed={collapsed} setCollapsed={setCollapsed} />
+        <ContentCtn collapsed={collapsed}>
+          <TotalFileCtn>
+            <TotalFileStatus 
+              background={background}
+              totalFiles={totalFiles}
+              uploadDate={uploadDate}
+              fileCount={fileCount}
+              color = {color}
+            />
+            <TotalFileStatus 
+              background={'#FFF0EE'}
+              totalFiles={'오류 파일 수'}
+              uploadDate={uploadDate}
+              fileCount={400}
+              color = {'#FF1500'}
+            />
+          </TotalFileCtn>
+        </ContentCtn>
+      </MainContent>
+    </AppContainer>
+  );
+};
+
+export default DataCompositionView;
 
 const AppContainer = styled.div`
   display: flex;
@@ -30,20 +72,11 @@ const ContentCtn = styled.div`
   overflow: hidden;
 `;
 
-
-const DataCompositionView = () => {
-  const [collapsed, setCollapsed] = useState(false);
- 
-  return (
-    <AppContainer>
-      <TopBar />
-      <MainContent>
-        <MenuBar collapsed={collapsed} setCollapsed={setCollapsed} />
-        <ContentCtn collapsed={collapsed}>
-        </ContentCtn>
-      </MainContent>
-    </AppContainer>
-  );
-};
-
-export default DataCompositionView;
+const TotalFileCtn = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  flex: 1;
+  margin-top: 3%;
+  gap: 20px;
+`;
