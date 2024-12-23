d4d45af798c1b03c76a083ab1ebf75702bb57fe4
DESKTOP-BSAE3OV\fasol <wlsdn4348@naver.com>
2024-12-23 11:14:37 +0900

    서버 빌드

--- src/api/fileUploadApi.js
+++ src/api/fileUploadApi.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: process.env.REACT_APP_API_URL_INTERNAL || process.env.REACT_APP_API_URL_EXTERNAL || 'http://localhost:8080',
+    baseURL: window.location.hostname === '202.86.11.19'
+      ? process.env.REACT_APP_API_URL_EXTERNAL // 외부망
+      : process.env.REACT_APP_API_URL_INTERNAL // 내부망
   });
-  
+
 // 다중 파일을 업로드하는 함수
 export const uploadExcelFiles = async (files) => {
     try {
@@ -14,7 +16,7 @@ export const uploadExcelFiles = async (files) => {
             formData.append(`files`, file); // 여러 파일을 'files'라는 이름으로 전송
         });
 
-        const response = await axios.post('http://localhost:8080/api/upload-folder', formData, {
+        const response = await apiClient.post('/api/upload-folder', formData, {
             headers: {
                 'Content-Type': 'multipart/form-data', // 파일을 업로드하는 요청임을 명시
             },
@@ -32,7 +34,7 @@ export const uploadExcelFiles = async (files) => {
 export const fetchPatientData = async (fileIds, institutionId, diseaseClass) => {
     try {
         console.log('전송할 데이터:', { fileIds, institutionId, diseaseClass }); 
-        const response = await axios.post('http://localhost:8080/api/analyze', {
+        const response = await apiClient.post('/api/analyze', {
             fileIds: fileIds,   // 배열로 전달
             institutionId: institutionId,
             diseaseClass: diseaseClass,
